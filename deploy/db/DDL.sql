d7b395b1ab90fde18878759ed4d1fbc2ff37fd36
mshen <mshen@odc.ustcsoft.com>
2018-12-13 23:10:47 +0800

    DDL

--- deploy/db/DDL.sql
+++ deploy/db/DDL.sql
@@ -14,6 +14,7 @@ CREATE TABLE `unit_seq` (
   PRIMARY KEY (`id`)
 );
 
+DROP TABLE IF EXISTS `radiation`;
 DROP TABLE IF EXISTS `sensor`;
 DROP TABLE IF EXISTS `unit`;
 CREATE TABLE `unit` (
@@ -30,11 +31,23 @@ CREATE TABLE `unit` (
   KEY `LEAF_IDX` (`LEAF`)
 );
 
+CREATE TABLE `radiation` (
+  `RAD_ID` BIGINT NOT NULL AUTO_INCREMENT,
+  `UNIT_ID` CHAR(16) NOT NULL,
+  `RAD_VALUE` VARCHAR(45) NULL,
+  PRIMARY KEY (`RAD_ID`),
+  INDEX `RAD_UNIT_ID_idx` (`UNIT_ID` ASC) VISIBLE,
+  CONSTRAINT `RAD_UNIT_ID`
+    FOREIGN KEY (`UNIT_ID`)
+    REFERENCES `unit` (`UNIT_ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
 DROP TRIGGER IF EXISTS `unit_BEFORE_INSERT`;
 DELIMITER $$
 CREATE TRIGGER `unit_BEFORE_INSERT` BEFORE INSERT ON `unit` FOR EACH ROW
 BEGIN
-	INSERT INTO unit_seq VALUES (NULL);
+    INSERT INTO unit_seq VALUES (NULL);
     SET NEW.UNIT_ID = CONCAT('UT', LPAD(LAST_INSERT_ID(), 14, '0'));
 END$$
 DELIMITER ;
@@ -92,6 +105,8 @@ DELIMITER ;
 
 INSERT INTO `unit` VALUES ('UT00000000000001','/',NULL,'系统根节点',1,0);
 INSERT INTO `user` VALUES ('000001','张三','zhs@test.org','','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','1234567890', 1);
+INSERT INTO threshold values ('RADIATION', 70, 50);
+
 
 DROP FUNCTION IF EXISTS `getUnitPath`;
 DELIMITER $$
@@ -99,22 +114,105 @@ CREATE FUNCTION `getUnitPath`(id char(16)) RETURNS varchar(2048) CHARSET utf8mb4
     READS SQL DATA
     DETERMINISTIC
 BEGIN
-	DECLARE currentPId char(16);  
-	DECLARE fullPath varchar(2048);
-	DECLARE tempName varchar(2048);
+    DECLARE currentPId char(16);  
+    DECLARE fullPath varchar(2048);
+    DECLARE tempName varchar(2048);
     SET tempName='';
 
-	SELECT `UNIT_NAME`, `PARENT_ID` INTO fullPath, currentPId FROM `unit` WHERE `UNIT_ID` = id AND ACTIVE=true;
+    SELECT `UNIT_NAME`, `PARENT_ID` INTO fullPath, currentPId FROM `unit` WHERE `UNIT_ID` = id AND ACTIVE=true;
 
-	WHILE currentPId IS NOT NULL DO
-		SELECT `UNIT_NAME`, `PARENT_ID` INTO tempName, currentPId FROM `unit` WHERE `UNIT_ID` = currentPId AND ACTIVE=true;
+    WHILE currentPId IS NOT NULL DO
+        SELECT `UNIT_NAME`, `PARENT_ID` INTO tempName, currentPId FROM `unit` WHERE `UNIT_ID` = currentPId AND ACTIVE=true;
         IF tempName <> '/' THEN
-			SET fullPath = CONCAT(tempName, '/', fullPath);
+            SET fullPath = CONCAT(tempName, '/', fullPath);
         ELSE
-			SET fullPath = CONCAT(tempName, fullPath);
-		END IF;
-	END WHILE;
+            SET fullPath = CONCAT(tempName, fullPath);
+        END IF;
+    END WHILE;
 
-	RETURN fullPath;
+    RETURN fullPath;
 END$$
+DELIMITER ;
+
+DROP procedure IF EXISTS `getRadiationUnitStatus`;
+DELIMITER $$
+CREATE PROCEDURE `getRadiationUnitStatus`(IN unitId CHAR(16), IN warn INT, IN err INT, OUT stats INT)
+BEGIN
+    DECLARE done BOOL DEFAULT false;
+    DECLARE radValue INT;
+    DECLARE ret INT DEFAULT 0;
+    DECLARE countWarn INT DEFAULT 0;
+    DECLARE countError INT DEFAULT 0;
+    
+    DECLARE curl CURSOR FOR SELECT `RAD_VALUE` FROM `RADIATION` WHERE `UNIT_ID`=unitId ORDER BY RAD_ID DESC LIMIT 0, 10;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = true;
+    
+    OPEN curl;
+    radLoop: LOOP
+        FETCH curl INTO radValue;
+        IF done THEN
+            SET ret = -1;
+            LEAVE radLoop;
+        ELSEIF radValue <= err THEN
+            SET countError = countError + 1;
+        ELSEIF radValue > err AND radValue <= warn THEN
+            SET countWarn = countWarn + 1;
+        END IF;
+    END LOOP radLoop;
+    CLOSE curl;
+    
+    IF countError >= 4 THEN
+        SET ret = 2;
+    ELSEIF countWarn >= 4 THEN
+        SET ret = 2;
+    END IF;
+    
+    SELECT ret INTO stats;
+END$$
+
+DELIMITER ;
+
+
+DROP procedure IF EXISTS `getUnitStatus`;
+DELIMITER $$
+CREATE PROCEDURE `getUnitStatus`(IN unitId CHAR(16), IN normal INT, IN warn INT, OUT stats INT)
+BEGIN
+    DECLARE done BOOL DEFAULT false;
+    DECLARE parentUnitId INT;
+    
+    DECLARE curl CURSOR FOR SELECT `UNIT_ID` FROM `UNIT` WHERE `PARENT_ID`=unitId;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = true;
+    
+    OPEN curl;
+    radLoop: LOOP
+        FETCH curl INTO parentUnitId;
+        IF done THEN
+            CALL getRadiationUnitStatus(unitId, normal, warn, stats);
+            LEAVE radLoop;
+        ELSE
+            CALL getUnitStatus(unitId, normal, warn, stats);
+        END IF;
+    END LOOP radLoop;
+    CLOSE curl;
+END$$
+
+DELIMITER ;
+
+
+DROP function IF EXISTS `getUnitStatus`;
+DELIMITER $$
+CREATE FUNCTION `getUnitStatus` (id CHAR(16)) RETURNS INT
+    READS SQL DATA
+    DETERMINISTIC
+BEGIN
+    DECLARE stats INT;
+    DECLARE normal INT;
+    DECLARE warn INT;
+    
+    SELECT `NORMAL`, `WARNING` INTO normal, warn FROM `THRESHOLD` WHERE `TYPE`='RADIATION';
+
+    CALL getUnitStatus(id, normal, warn, stats);
+    RETURN stats;
+END$$
+
 DELIMITER ;
