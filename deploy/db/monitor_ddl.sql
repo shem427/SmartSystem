1115ead6654a9f7db4e0fc58e989a81eb789e017
shen <shen@gts.org>
2019-04-08 00:01:29 +0800

    ddl

--- deploy/db/monitor_ddl.sql
+++ deploy/db/monitor_ddl.sql
@@ -1,150 +1,235 @@
 /*
- Navicat MySQL Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : local
- Source Server Type    : MySQL
- Source Server Version : 50643
- Source Host           : localhost:3306
- Source Schema         : monitor
+Source Server         : monitor
+Source Server Version : 50725
+Source Host           : localhost:3306
+Source Database       : monitor
 
- Target Server Type    : MySQL
- Target Server Version : 50643
- File Encoding         : 65001
+Target Server Type    : MYSQL
+Target Server Version : 50725
+File Encoding         : 65001
 
- Date: 01/04/2019 09:33:08
+Date: 2019-04-08 00:00:06
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for radiation
 -- ----------------------------
 DROP TABLE IF EXISTS `radiation`;
-CREATE TABLE `radiation`  (
+CREATE TABLE `radiation` (
   `RAD_ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `UNIT_ID` char(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `RAD_VALUE` int(11) NULL DEFAULT NULL,
+  `UNIT_ID` char(16) NOT NULL,
+  `RAD_VALUE` int(11) DEFAULT NULL,
   PRIMARY KEY (`RAD_ID`) USING BTREE,
-  INDEX `RAD_UNIT_ID`(`UNIT_ID`) USING BTREE,
+  KEY `RAD_UNIT_ID` (`UNIT_ID`) USING BTREE,
   CONSTRAINT `RAD_UNIT_ID` FOREIGN KEY (`UNIT_ID`) REFERENCES `unit` (`UNIT_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB AUTO_INCREMENT = 104 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+) ENGINE=InnoDB AUTO_INCREMENT=193 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Table structure for sensor
 -- ----------------------------
 DROP TABLE IF EXISTS `sensor`;
-CREATE TABLE `sensor`  (
-  `SENSOR_ID` char(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'SS',
-  `RADIATION_MODEL_ID` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `SENSOR_NAME` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `UNIT_ID` char(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `SENSOR_REMARK` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `SENSOR_SN` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `SENSOR_MODEL` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `ACTIVE` tinyint(1) NOT NULL DEFAULT 1,
+CREATE TABLE `sensor` (
+  `SENSOR_ID` char(16) NOT NULL DEFAULT 'SS',
+  `RADIATION_MODEL_ID` varchar(32) DEFAULT NULL,
+  `SENSOR_NAME` varchar(128) NOT NULL,
+  `UNIT_ID` char(16) NOT NULL,
+  `SENSOR_REMARK` varchar(1024) DEFAULT NULL,
+  `SENSOR_SN` varchar(128) DEFAULT NULL,
+  `SENSOR_MODEL` varchar(64) DEFAULT NULL,
+  `ACTIVE` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`SENSOR_ID`) USING BTREE,
-  UNIQUE INDEX `RADIATION_MODEL_ID_idx`(`RADIATION_MODEL_ID`) USING BTREE,
-  INDEX `SENSOR_UNIT_ID_idx`(`UNIT_ID`) USING BTREE,
-  CONSTRAINT `SENSOR_UNIT_ID` FOREIGN KEY (`UNIT_ID`) REFERENCES `unit` (`UNIT_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+  UNIQUE KEY `RADIATION_MODEL_ID_idx` (`RADIATION_MODEL_ID`) USING BTREE,
+  KEY `SENSOR_UNIT_ID_idx` (`UNIT_ID`) USING BTREE,
+  CONSTRAINT `SENSOR_UNIT_ID` FOREIGN KEY (`UNIT_ID`) REFERENCES `unit` (`UNIT_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
+
+-- ----------------------------
+-- Table structure for sensor_data
+-- ----------------------------
+DROP TABLE IF EXISTS `sensor_data`;
+CREATE TABLE `sensor_data` (
+  `DATA_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `RADIATION_MODEL_ID` varchar(32) CHARACTER SET utf8mb4 NOT NULL,
+  `DATA_VALUE` int(11) NOT NULL,
+  `DATA_TIME` timestamp NOT NULL,
+  PRIMARY KEY (`DATA_ID`),
+  KEY `sensor_id_fk` (`RADIATION_MODEL_ID`),
+  CONSTRAINT `sensor_id_fk` FOREIGN KEY (`RADIATION_MODEL_ID`) REFERENCES `sensor` (`RADIATION_MODEL_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- ----------------------------
 -- Table structure for sensor_seq
 -- ----------------------------
 DROP TABLE IF EXISTS `sensor_seq`;
-CREATE TABLE `sensor_seq`  (
+CREATE TABLE `sensor_seq` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Table structure for threshold
 -- ----------------------------
 DROP TABLE IF EXISTS `threshold`;
-CREATE TABLE `threshold`  (
-  `DATA_ID` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+CREATE TABLE `threshold` (
+  `DATA_ID` varchar(32) NOT NULL,
   `NORML` int(11) NOT NULL,
   `WARN` int(11) NOT NULL,
   PRIMARY KEY (`DATA_ID`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Table structure for unit
 -- ----------------------------
 DROP TABLE IF EXISTS `unit`;
-CREATE TABLE `unit`  (
-  `UNIT_ID` char(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'UT00000000000000',
-  `UNIT_NAME` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `PARENT_ID` char(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `REMARK` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `ACTIVE` tinyint(1) NOT NULL DEFAULT 1,
-  `LEAF` tinyint(1) NOT NULL DEFAULT 0,
-  `UNIT_STATUS` int(2) NOT NULL DEFAULT -1,
+CREATE TABLE `unit` (
+  `UNIT_ID` char(16) NOT NULL DEFAULT 'UT00000000000000',
+  `UNIT_NAME` varchar(128) NOT NULL,
+  `PARENT_ID` char(16) DEFAULT NULL,
+  `REMARK` varchar(256) DEFAULT NULL,
+  `ACTIVE` tinyint(1) NOT NULL DEFAULT '1',
+  `LEAF` tinyint(1) NOT NULL DEFAULT '0',
+  `UNIT_STATUS` int(2) NOT NULL DEFAULT '-1',
   PRIMARY KEY (`UNIT_ID`) USING BTREE,
-  INDEX `PARENT_ID_INX`(`PARENT_ID`) USING BTREE,
-  INDEX `UNIT_NAME_IDX`(`UNIT_NAME`) USING BTREE,
-  INDEX `ACTIVE_IDX`(`ACTIVE`) USING BTREE,
-  INDEX `LEAF_IDX`(`LEAF`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+  KEY `PARENT_ID_INX` (`PARENT_ID`) USING BTREE,
+  KEY `UNIT_NAME_IDX` (`UNIT_NAME`) USING BTREE,
+  KEY `ACTIVE_IDX` (`ACTIVE`) USING BTREE,
+  KEY `LEAF_IDX` (`LEAF`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Table structure for unit_manager
 -- ----------------------------
 DROP TABLE IF EXISTS `unit_manager`;
-CREATE TABLE `unit_manager`  (
-  `UNIT_ID` char(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `USER_ID` char(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`UNIT_ID`, `USER_ID`) USING BTREE,
-  INDEX `UNIT_ID_IDX`(`UNIT_ID`) USING BTREE,
-  INDEX `MANAGER_ID_IDX`(`USER_ID`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+CREATE TABLE `unit_manager` (
+  `UNIT_ID` char(16) NOT NULL,
+  `USER_ID` char(6) NOT NULL,
+  PRIMARY KEY (`UNIT_ID`,`USER_ID`) USING BTREE,
+  KEY `UNIT_ID_IDX` (`UNIT_ID`) USING BTREE,
+  KEY `MANAGER_ID_IDX` (`USER_ID`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Table structure for unit_seq
 -- ----------------------------
 DROP TABLE IF EXISTS `unit_seq`;
-CREATE TABLE `unit_seq`  (
+CREATE TABLE `unit_seq` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Table structure for unit_warn
 -- ----------------------------
 DROP TABLE IF EXISTS `unit_warn`;
-CREATE TABLE `unit_warn`  (
+CREATE TABLE `unit_warn` (
   `WARN_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `UNIT_ID` char(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `UNIT_ID` char(16) NOT NULL,
   `UNIT_STATUS` int(2) NOT NULL,
-  `NOTIFY_TIME` datetime(0) NOT NULL,
+  `NOTIFY_TIME` datetime NOT NULL,
   PRIMARY KEY (`WARN_ID`) USING BTREE,
-  INDEX `UNIT_ID_WARN_idx`(`UNIT_ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+  KEY `UNIT_ID_WARN_idx` (`UNIT_ID`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=373 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
-  `USER_ID` char(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `USER_NAME` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `MAIL_ADDRESS` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `USER_ROLES` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `USER_PASSWORD` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `PHONE_NUMBER` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+CREATE TABLE `user` (
+  `USER_ID` char(6) NOT NULL,
+  `USER_NAME` varchar(64) NOT NULL,
+  `MAIL_ADDRESS` varchar(64) NOT NULL,
+  `USER_ROLES` varchar(128) DEFAULT NULL,
+  `USER_PASSWORD` varchar(64) NOT NULL,
+  `PHONE_NUMBER` varchar(20) NOT NULL,
   `ACTIVE` tinyint(1) NOT NULL,
   PRIMARY KEY (`USER_ID`) USING BTREE,
-  INDEX `USERNAME`(`USER_NAME`) USING BTREE,
-  INDEX `USERMAIL`(`MAIL_ADDRESS`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+  KEY `USERNAME` (`USER_NAME`) USING BTREE,
+  KEY `USERMAIL` (`MAIL_ADDRESS`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
+
+-- ----------------------------
+-- Procedure structure for countLightByStatus
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `countLightByStatus`;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `countLightByStatus`(IN `parentUnitId` char(16),IN `stats` int,OUT `size` int)
+BEGIN
+    DECLARE done BOOL DEFAULT false;
+    DECLARE cont INT DEFAULT 0;
+    DECLARE unitId CHAR(16);
+    DECLARE unitStatus INT(11);
+    DECLARE unitLeaf BOOL;
+    DECLARE curl CURSOR FOR SELECT `UNIT_ID`,`UNIT_STATUS`,`LEAF` FROM `UNIT` WHERE `ACTIVE`=true AND `PARENT_ID`=parentUnitId;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = true;
+    SET @@max_sp_recursion_depth = 100;
+
+    OPEN curl;
+    uLoop: LOOP
+        FETCH curl INTO unitId,unitStatus,unitLeaf;
+        IF done THEN
+            LEAVE uLoop;
+        ELSEIF unitLeaf AND unitStatus = stats THEN
+            SET cont = cont + 1;
+        ELSE
+            CALL countLightByStatus(unitId, stats, @t_cont);
+            SET cont = cont + @t_cont;
+        END IF;
+    END LOOP uLoop;
+    CLOSE curl;
+    SELECT cont INTO size;
+END
+;;
+DELIMITER ;
+
+-- ----------------------------
+-- Procedure structure for countSensor
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `countSensor`;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `countSensor`(IN `parentUnitId` char(16),IN `inactiveHours` int,OUT `activeCount` int,OUT `inactiveCount` int)
+BEGIN
+    DECLARE done BOOL DEFAULT false;
+    DECLARE activeSize INT DEFAULT 0;
+    DECLARE inactiveSize INT DEFAULT 0;
+    DECLARE unitId CHAR(16);
+    DECLARE unitLeaf BOOL;
+    DECLARE curl CURSOR FOR SELECT `UNIT_ID`,`LEAF` FROM `UNIT` WHERE `ACTIVE`=true AND `PARENT_ID`=parentUnitId;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = true;
+    SET @@max_sp_recursion_depth = 10;
+
+    OPEN curl;
+    uLoop: LOOP
+        FETCH curl INTO unitId,unitLeaf;
+        IF done THEN
+            LEAVE uLoop;
+        ELSEIF unitLeaf THEN
+            SELECT COUNT(1) INTO @t_count FROM (SELECT MAX(D.`DATA_TIME`) AS LAST_TIME FROM `SENSOR_DATA` D, `SENSOR` S WHERE D.`RADIATION_MODEL_ID`=S.`RADIATION_MODEL_ID` AND S.`UNIT_ID`=unitId GROUP BY D.`RADIATION_MODEL_ID`) T WHERE TIMESTAMPDIFF(HOUR, T.`LAST_TIME`, NOW()) < inactiveHours;
+            SET activeSize = activeSize + @t_count;
+            SELECT COUNT(1) INTO @t_count FROM (SELECT MAX(D.`DATA_TIME`) AS LAST_TIME FROM `SENSOR_DATA` D, `SENSOR` S WHERE D.`RADIATION_MODEL_ID`=S.`RADIATION_MODEL_ID` AND S.`UNIT_ID`=unitId GROUP BY D.`RADIATION_MODEL_ID`) T WHERE TIMESTAMPDIFF(HOUR, T.`LAST_TIME`, NOW()) >= inactiveHours;
+            SET inactiveSize = inactiveSize + @t_count;
+        ELSE
+            CALL countSensor(unitId, inactiveHours, @activeCount, @inactiveCount);
+            SET activeSize = activeSize + @activeCount;
+            SET inactiveSize = inactiveSize + @inactiveCount;
+        END IF;
+    END LOOP uLoop;
+    CLOSE curl;
+    SELECT activeSize INTO activeCount;
+    SELECT inactiveSize INTO inactiveCount;
+END
+;;
+DELIMITER ;
 
 -- ----------------------------
 -- Procedure structure for getRadiationUnitStatus
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `getRadiationUnitStatus`;
-delimiter ;;
-CREATE PROCEDURE `getRadiationUnitStatus`(IN unitId CHAR(16), IN norml INT, IN warn INT, OUT stats INT)
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getRadiationUnitStatus`(IN unitId CHAR(16), IN norml INT, IN warn INT, OUT stats INT)
 BEGIN
     DECLARE ret INT DEFAULT 0;
     DECLARE countTotal INT DEFAULT 0;
@@ -168,14 +253,14 @@ BEGIN
     SELECT ret INTO stats;
 END
 ;;
-delimiter ;
+DELIMITER ;
 
 -- ----------------------------
 -- Procedure structure for getUnitByManagerParentId
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `getUnitByManagerParentId`;
-delimiter ;;
-CREATE PROCEDURE `getUnitByManagerParentId`(IN userId CHAR(6), IN unitParentId CHAR(16))
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getUnitByManagerParentId`(IN userId CHAR(6), IN unitParentId CHAR(16))
 BEGIN
     DECLARE done BOOL DEFAULT false;
     DECLARE unitId CHAR(16);
@@ -195,48 +280,17 @@ BEGIN
     CLOSE curl;
 END
 ;;
-delimiter ;
-
--- ----------------------------
--- Function structure for getUnitPath
--- ----------------------------
-DROP FUNCTION IF EXISTS `getUnitPath`;
-delimiter ;;
-CREATE FUNCTION `getUnitPath`(id char(16))
- RETURNS varchar(2048) CHARSET utf8mb4
-  READS SQL DATA 
-  DETERMINISTIC
-BEGIN
-    DECLARE currentPId char(16);  
-    DECLARE fullPath varchar(2048);
-    DECLARE tempName varchar(2048);
-    SET tempName='';
-
-    SELECT `UNIT_NAME`, `PARENT_ID` INTO fullPath, currentPId FROM `unit` WHERE `UNIT_ID` = id AND ACTIVE=true;
-
-    WHILE currentPId IS NOT NULL DO
-        SELECT `UNIT_NAME`, `PARENT_ID` INTO tempName, currentPId FROM `unit` WHERE `UNIT_ID` = currentPId AND ACTIVE=true;
-        IF tempName <> '/' THEN
-            SET fullPath = CONCAT(tempName, '/', fullPath);
-        ELSE
-            SET fullPath = CONCAT(tempName, fullPath);
-        END IF;
-    END WHILE;
-
-    RETURN fullPath;
-END
-;;
-delimiter ;
+DELIMITER ;
 
 -- ----------------------------
 -- Procedure structure for getUnitUntilParentId
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `getUnitUntilParentId`;
-delimiter ;;
-CREATE PROCEDURE `getUnitUntilParentId`(IN unitId CHAR(16), IN unitParentId CHAR(16))
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getUnitUntilParentId`(IN unitId CHAR(16), IN unitParentId CHAR(16))
 BEGIN
     DECLARE pId CHAR(16);
-	SET @@max_sp_recursion_depth = 100;
+    SET @@max_sp_recursion_depth = 100;
     SELECT `PARENT_ID` INTO pId FROM `unit` WHERE `ACTIVE` = true AND `UNIT_ID`=unitId;
     IF unitParentId=pId THEN
         SELECT `UNIT_ID`, `UNIT_NAME`, `PARENT_ID`, `REMARK`, `UNIT_STATUS` FROM `unit` WHERE `ACTIVE` = true AND `UNIT_ID`=unitId;
@@ -245,14 +299,14 @@ BEGIN
     END IF;
 END
 ;;
-delimiter ;
+DELIMITER ;
 
 -- ----------------------------
 -- Procedure structure for updateParentStatus
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `updateParentStatus`;
-delimiter ;;
-CREATE PROCEDURE `updateParentStatus`(IN unitId CHAR(16), IN stats INT)
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updateParentStatus`(IN unitId CHAR(16), IN stats INT)
 BEGIN
     DECLARE pId CHAR(16);
     DECLARE pStatus INT;
@@ -262,25 +316,25 @@ BEGIN
         SELECT `UNIT_STATUS` INTO pStatus FROM `UNIT` WHERE `ACTIVE`=true AND `UNIT_ID`=pId;    
         IF pStatus < stats THEN
             UPDATE `UNIT` SET `UNIT_STATUS`=stats WHERE `UNIT_ID`=pId;
-		ELSE
+        ELSE
             SET stats=pStatus;
         END IF;
         SELECT `PARENT_ID` INTO pId FROM `UNIT` WHERE `ACTIVE`=true AND `UNIT_ID`=pId;
-	END WHILE;
+    END WHILE;
 END
 ;;
-delimiter ;
+DELIMITER ;
 
 -- ----------------------------
 -- Procedure structure for updateUnitStatus
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `updateUnitStatus`;
-delimiter ;;
-CREATE PROCEDURE `updateUnitStatus`()
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updateUnitStatus`()
 BEGIN
     DECLARE done BOOL DEFAULT false;
-	DECLARE stats INT;
-	DECLARE norml INT;
+    DECLARE stats INT;
+    DECLARE norml INT;
     DECLARE warn INT;
     DECLARE thresholdId varchar(64);
     DECLARE leafUnitId CHAR(16);
@@ -304,18 +358,42 @@ BEGIN
     CLOSE curl;    
 END
 ;;
-delimiter ;
+DELIMITER ;
 
 -- ----------------------------
--- Triggers structure for table sensor
+-- Function structure for getUnitPath
 -- ----------------------------
+DROP FUNCTION IF EXISTS `getUnitPath`;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `getUnitPath`(id char(16)) RETURNS varchar(2048) CHARSET utf8mb4 COLLATE utf8mb4_bin
+    READS SQL DATA
+    DETERMINISTIC
+BEGIN
+    DECLARE currentPId char(16);  
+    DECLARE fullPath varchar(2048);
+    DECLARE tempName varchar(2048);
+    SET tempName='';
+
+    SELECT `UNIT_NAME`, `PARENT_ID` INTO fullPath, currentPId FROM `unit` WHERE `UNIT_ID` = id AND ACTIVE=true;
+
+    WHILE currentPId IS NOT NULL DO
+        SELECT `UNIT_NAME`, `PARENT_ID` INTO tempName, currentPId FROM `unit` WHERE `UNIT_ID` = currentPId AND ACTIVE=true;
+        IF tempName <> '/' THEN
+            SET fullPath = CONCAT(tempName, '/', fullPath);
+        ELSE
+            SET fullPath = CONCAT(tempName, fullPath);
+        END IF;
+    END WHILE;
+
+    RETURN fullPath;
+END
+;;
+DELIMITER ;
 DROP TRIGGER IF EXISTS `sensor_BEFORE_INSERT`;
-delimiter ;;
+DELIMITER ;;
 CREATE TRIGGER `sensor_BEFORE_INSERT` BEFORE INSERT ON `sensor` FOR EACH ROW BEGIN
     INSERT INTO sensor_seq VALUES (NULL);
     SET NEW.SENSOR_ID = CONCAT('SS', LPAD(LAST_INSERT_ID(), 14, '0'));
 END
 ;;
-delimiter ;
-
-SET FOREIGN_KEY_CHECKS = 1;
+DELIMITER ;
